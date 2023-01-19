9486cca3783d7ae5be86c75f3479086b6afcf181
Frederick Bruce <frederick.bruce.dev@gmail.com>
2023-01-19 12:03:43 -0500

    refactor

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/destructuring-assignment */
-import { useContext } from "react";
 import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
 import InputBase from "@mui/material/InputBase";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
@@ -17,14 +17,18 @@ const Topbar = () => {
 
   return (
     <Box display="flex" justifyContent="space-between" p={2}>
-      {/* SearchBAr */}
-      <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
+      {/* SEARCH BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
         <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
         <IconButton type="button" sx={{ p: 1 }}>
           <SearchIcon />
         </IconButton>
       </Box>
-      {/* ICONS */}
+
       {/* ICONS */}
       <Box display="flex">
         <IconButton onClick={colorMode.toggleColorMode}>
